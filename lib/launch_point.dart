91d0f328a49fc2385dec76a7abb57fa951d13595
Mostafa Morsy <mostafa.morsy@intalio.com>
2025-04-06 06:15:05 +0200

    update: structure of launch app

---
+++ lib/launch_point.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mostafamorsy/config/theme/light_theme/light_theme.dart';
+
+import 'feature/home/presentation/ui/screen/homeScreen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: LightTheme.lightTheme,
+      home: const HomeScreen(),
+    );
+  }
+}
